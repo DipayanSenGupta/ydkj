9a07b834c86471b7084e3843aa4331a6f2e2a25c
dip.himu@gmail.com <dip.himu@gmail.com>
2019-07-20 21:24:27 +0600

    Class file changed to only functions

--- ch1/challenge-1.js
+++ ch1/challenge-1.js
@@ -1,27 +1,14 @@
-class PhonePurchase {
-	constructor (bankAccount, phonePrice, headPhonePrice, chargerPrice) {
-		this._bankAccount = bankAccount
-		this._phonePrice = phonePrice
-		this._headPhonePrice = headPhonePrice
-		this._chargerPrice = chargerPrice
-	}
 
-calculateTax(price, tax){
-		return price = (price*tax)/100
-	}
 
-formatingPrice(price){
-		return '$ ' + price.toFixed()
-	}
- phonePurchaseAction () {
+	function  phonePurchaseAction (bankAccount,headPhonePrice, chargerPrice, phonePrice,) {
 		var count = 0
 		var set = [1, 2, 3]
 		var buyinglog = [0, 0, 0]
 		var tax = [5, 7, 10]
 		var totalPrice = [0, 0, 0]
-		var minimum = Math.min(this._phonePrice, this._headPhonePrice, this._chargerPrice)
+		var minimum = Math.min(phonePrice, headPhonePrice, chargerPrice)
 
-		for (count = 0; this._bankAccount >= minimum; count++) {
+		for (count = 0; bankAccount >= minimum; count++) {
 
 			var rndm = Math.floor(Math.random() * set.length)
 			var userPreference = set[rndm]
@@ -30,25 +17,25 @@ formatingPrice(price){
 			switch (userPreference) {
 				case 1:
 
-				headPhoneCost = this._headPhonePrice + calculateTax(this._headPhonePrice * tax[0])
-				if( (this._bankAccount - headPhoneCost) > 0 ) {
-					this._bankAccount -= headPhoneCost
+				headPhoneCost = headPhonePrice + calculateTax(headPhonePrice * tax[0])
+				if( (bankAccount - headPhoneCost) > 0 ) {
+					bankAccount -= headPhoneCost
 					totalPrice[0] += headPhoneCost
 					buyinglog[0]++
 				}
 				break
 				case 2:
-				chargerCost = this._chargerPrice + calculateTax(this._chargerPrice * tax[1])
-				if( (this._bankAccount - chargerCost) > 0 ) {
-					this._bankAccount -= chargerCost
+				chargerCost = chargerPrice + calculateTax(chargerPrice * tax[1])
+				if( (bankAccount - chargerCost) > 0 ) {
+					bankAccount -= chargerCost
 					totalPrice[1] += chargerCost
 					buyinglog[1]++
 				}
 				break
 				case 3:
-				phonePriceCost = this._phonePrice + calculateTax(this._phonePrice * tax[2])
-				if( (this._bankAccount - phonePriceCost) > 0 ) {
-					this._bankAccount -= phonePriceCost
+				phonePriceCost = phonePrice + calculateTax(phonePrice * tax[2])
+				if( (bankAccount - phonePriceCost) > 0 ) {
+					bankAccount -= phonePriceCost
 					totalPrice[2] += phonePriceCost
 					buyinglog[2]++
 				}
@@ -65,9 +52,20 @@ formatingPrice(price){
 		console.log('Amount cost in buying headphone ' + formatingPrice(totalPrice[2]) +' ( tax included )')
 		var totalCost = totalPrice[0] +totalPrice[1]+totalPrice[2]
 		console.log('Total cost '+ formatingPrice(totalCost) )
-		console.log('Bank balance left '+ formatingPrice(this._bankAccount) )
+		console.log('Bank balance left '+ formatingPrice(bankAccount) )
+
+
+		function calculateTax(price, tax){
+			var price = (price*tax)/100
+			console.log(price);
+			return price
+		}
+
+		function formatingPrice(price){
+			var formattedPrice ='$ ' + price.toFixed()
+			console.log(formattedPrice);
+			return formattedPrice
+		}
 
 	}
-}
-var obj = new PhonePurchase(1005, 10, 20, 40)
-obj.phonePurchaseAction()
+phonePurchaseAction(1005, 10, 20, 40)
